9dd77837eac51f4a1895717fedd76071f182b27d
Mateusz <borko.m.126@gmail.com>
2023-05-20 16:59:22 +0200

    Min and Max added as parameters for response

--- src/main/java/com/example/learnacountries/controller/MainController.java
+++ src/main/java/com/example/learnacountries/controller/MainController.java
@@ -1,7 +1,8 @@
 package com.example.learnacountries.controller;
 
+import com.example.learnacountries.dto.ContinentResponseDto;
 import com.example.learnacountries.model.Continent;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequiredArgsConstructor
 public class MainController {
 
+    @Value("${countries.retrieve.min-count}")
+    private int min;
+
+    @Value("${countries.retrieve.max-count}")
+    private int max;
+
     @GetMapping("/")
-    public ResponseEntity<List<String>> home() {
-        return ResponseEntity.ok(Continent.getAllContinentNames());
+    public ResponseEntity<ContinentResponseDto> home() {
+        List<String> continentNames = Continent.getAllContinentNames();
+        ContinentResponseDto responseData = new ContinentResponseDto(continentNames, min, max);
+        return ResponseEntity.ok(responseData);
     }
 
 
 }
+
